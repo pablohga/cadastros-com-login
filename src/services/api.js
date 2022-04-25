c997524675be1527661e2f7a00b6885c270da0f6
Pablo Azevedo <39193515+pablohga@users.noreply.github.com>
2022-04-25 01:48:08 +0100

    Alteração de URL da api para deploy na Vercel.com

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api'
+  /*baseURL: 'http://localhost:3000/api'*/
+  baseURL: 'https://cadastros-com-login-b6ycd5sfx-wildgadget.vercel.app'
 });
 
 export default api;
