2b0e5d08e994bbe9dc90fe13017801399ecaf094
Pablo Azevedo <39193515+pablohga@users.noreply.github.com>
2022-04-25 01:51:08 +0100

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   /*baseURL: 'http://localhost:3000/api'*/
-  baseURL: 'https://cadastros-com-login-b6ycd5sfx-wildgadget.vercel.app'
+  baseURL: 'https://cadastros-com-login-b6ycd5sfx-wildgadget.vercel.app/api'
 });
 
 export default api;
