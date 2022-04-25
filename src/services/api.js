4307df58dc925023b3707c4afc9635326614c263
Pablo Azevedo <39193515+pablohga@users.noreply.github.com>
2022-04-25 13:35:48 +0100

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-
+axios.defaults.headers.post['Content-Type'] =
+  'application/x-www-form-urlencoded';
 const api = axios.create({
   baseURL: 'http://localhost:3000/api'
  /* baseURL: 'https://cadastros-com-login-b6ycd5sfx-wildgadget.vercel.app/api'*/
